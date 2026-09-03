8fc6ee42e413a78da996b6133d80f8e7f72fcb87
selpats <71388521+selpats@users.noreply.github.com>
2026-09-03 13:02:41 +0400

    Limit parallel runner to at most 8 threads, bump version to 0.3.2

--- src/runner.rs
+++ src/runner.rs
@@ -1,16 +1,39 @@
+use std::sync::OnceLock;
+
 use jxl::api::{JxlParallelRunner, JxlParallelRunnerFun};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+static THREAD_POOL: OnceLock<rayon::ThreadPool> = OnceLock::new();
 
 pub struct RayonParallelRunner;
 
 impl JxlParallelRunner for RayonParallelRunner {
     fn run(&mut self, num: usize, fun: &JxlParallelRunnerFun) -> jxl::error::Result<()> {
-        if num == 1 || rayon::current_num_threads() == 1 {
+        if num == 1 {
             for i in 0..num {
                 fun(i)?;
             }
             return Ok(());
         }
-        (0..num).into_par_iter().try_for_each(fun)
+
+        let pool = THREAD_POOL.get_or_init(|| {
+            let threads = std::thread::available_parallelism()
+                .map(|n| n.get())
+                .unwrap_or(1)
+                .min(8);
+            rayon::ThreadPoolBuilder::new()
+                .num_threads(threads)
+                .build()
+                .unwrap()
+        });
+
+        if pool.current_num_threads() == 1 {
+            for i in 0..num {
+                fun(i)?;
+            }
+            return Ok(());
+        }
+
+        pool.install(|| (0..num).into_par_iter().try_for_each(fun))
     }
 }
